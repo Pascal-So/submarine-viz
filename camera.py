5571aedcba7a2406cf136f0201b9dca9a5ad37a4
Pascal Sommer <p@pascalsommer.ch>
2016-11-13 18:19:04 +0100

    Camera horizontal motion

--- camera.py
+++ camera.py
@@ -2,12 +2,54 @@ import bge
 controller = bge.logic.getCurrentController()
 move = controller.actuators["move"]
 
-press_w = cont.sensors["w"]
-press_a = cont.sensors["a"]
-press_s = cont.sensors["s"]
-press_d = cont.sensors["d"]
-wheel_up = cont.sensors["wheel up"]
-wheel_down = cont.sensors["wheel down"]
+press_w = controller.sensors["w"]
+press_a = controller.sensors["a"]
+press_s = controller.sensors["s"]
+press_d = controller.sensors["d"]
+wheel_up = controller.sensors["wheel up"]
+wheel_down = controller.sensors["wheel down"]
+
+def sign(x):
+    if (x > 0):
+        return 1
+    elif (x<0):
+        return -1
+    else:
+        return 0
 
 v_x = move.dLoc[0]
 v_y = move.dLoc[1]
+
+# acceleration factor of the camera
+acc = 0.2
+# max velocity of the camera (per axis)
+v_max = 1
+# dampening factor for the camera motion
+damp = 0.9
+# stop the velocity if it is smaller than this value
+stop_limit = 0.05
+
+a_x = acc * (press_d.positive - press_a.positive)
+a_y = acc * (press_w.positive - press_s.positive) 
+
+v_x += a_x
+v_y += a_y
+
+v_x *= damp
+v_y *= damp
+
+if(abs(v_x) < stop_limit):
+    v_x = 0
+if(abs(v_y) < stop_limit):
+    v_y = 0
+    
+if(abs(v_x) > v_max):
+    v_x = v_max*sign(v_x)
+if(abs(v_y) > v_max):
+    v_y = v_max*sign(v_y)
+    
+#print(v_x, v_y)
+#print(-True)
+    
+move.dLoc = [v_x, v_y, 0.0]
+controller.activate(move)
