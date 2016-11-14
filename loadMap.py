f44f8f3c95b9c58d77954a2df67f1ab2b6259121
Pascal Sommer <p@pascalsommer.ch>
2016-11-14 22:12:48 +0100

    import map

--- loadMap.py
+++ loadMap.py
@@ -7,27 +7,64 @@ import itertools
 #add_end = controller.actuators["add_end"]
 #add_start = controller.actuators["add_start"]
 
+terrain_types = {}
+terrain_types[0] = "start"
+terrain_types[1] = "end"
+terrain_types[2] = "water"
+terrain_types[3] = "land"
+
 
 def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    """Collect data into fixed-length chunks or blocks
+    
+    Example: grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    returns list of tuples
+    """
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 
 def create_map(width, height, pixels):
-    "place the land, start and end blocks, resize water"
+    """place the land, start and end blocks, resize water
+    
+    The coordinates are mapped one to one, this means that
+    one unit in the map data will correspond to one blender
+    unit.
+    Doesn't return anything, just has side effects.
+    """
+    
 
     scene = bge.logic.getCurrentScene()
     
-    def place_object(object, position):
-        object = scene.addObject(object, "gameLogic")
-        object.worldPosition(position)
+    def index_to_xy(i, width, height):
+        """ Takes 0 based index going line wise from top
+        left to bottom right, returns x, y coordinates so
+        that 0,0 is on bottom left corner
+        """
+        x = i % width
+        y = i // width
+        y*= -1
+        y+= height - 1
+        return (x,y)
     
-    
-    
+    def place_terrain(type, i):
+        """This won't return anything, just do side effects
+        
+        The object "gameLogic" is used to place the object
+        initially. It doesn't matter where this object is,
+        as long as it exists. There must be an easier way,
+        but this works.
+        """
+        x,y = index_to_xy(i, width, height)
 
+        object_name = terrain_types.get(type, "water")
+
+        if object_name != "water":
+            object = scene.addObject(object_name, "gameLogic")
+            object.worldPosition = (x,y,0) 
     
+    
+    list(map( (lambda tup : place_terrain(tup[1], tup[0])), list(enumerate(pixels)) ))
 
 
 def load_map(map_name):
