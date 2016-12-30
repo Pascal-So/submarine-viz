d461fbbfa0d24c26dc9131edd94d55013e3607d5
SmBe19 <git@schmidb.ch>
2016-12-30 22:18:09 +0100

    add director

--- camera.py
+++ camera.py
@@ -1,8 +1,14 @@
 import bge
 controller = bge.logic.getCurrentController()
 move = controller.actuators["move"]
-
 cam = controller.owner
+
+turn_phase_length = 4
+move_phase_length = 8
+
+
+# ^^^^^^^^^^^^ adjust speed settings here ^^^^^^^^^^^^^^^^^
+total_phase_length = turn_phase_length + move_phase_length
 
 # get sensor data
 press_w = controller.sensors["w"]
@@ -11,6 +17,7 @@ press_s = controller.sensors["s"]
 press_d = controller.sensors["d"]
 wheel_up = controller.sensors["wheel up"]
 wheel_down = controller.sensors["wheel down"]
+space = controller.sensors["space"]
 
 cam_height = cam.position[2]
 
@@ -45,14 +52,14 @@ stop_limit *= cam_height
 
 # calculate the acceleration and new velocity
 a_x = acc * (press_d.positive - press_a.positive)
-a_y = acc * (press_w.positive - press_s.positive) 
+a_y = acc * (press_w.positive - press_s.positive)
 v_x += a_x
 v_y += a_y
 v_x *= damp
 v_y *= damp
 
 
-# velocity limits 
+# velocity limits
 if(abs(v_x) < stop_limit):
     v_x = 0
 if(abs(v_y) < stop_limit):
@@ -75,5 +82,49 @@ v_z = v_speed * cam_height * (wheel_down.positive - wheel_up.positive)
 if( cam_height < camera_min_height and v_z < 0):
     v_z = 0
 
+############# directors ##############
+def lerp(val1, val2, frac):
+    """ linear interpolation """
+    return val1 * (1-frac) + val2 * frac
+
+def lerp_angle(val1, val2, frac):
+    """ like lerp, but wrap over 360 if it
+    is shorter
+    """
+    if abs(val2 - val1) <= 180:
+        return lerp(val1, val2, frac)
+    else:
+        if val1 < val2:
+            val1 += 360
+        else:
+            val2 += 360
+        return wrap(0,360, lerp(val1, val2, frac))
+
+frame_nr = cam["frame_nr"]
+neutval = [0] * 7
+oldval = neutval
+newval = neutval
+rotation_euler = cam.orientation.to_euler()
+curval = [cam["frame_nr_exact"] / total_phase_length, cam.position.x, cam.position.y, cam.position.z, rotation_euler.x, rotation_euler.y, rotation_euler.z]
+shouldval = neutval
+vr_x = vr_y = vr_z = 0
+for d in cam["directors"]:
+    if d[0] > frame_nr:
+        newval = d
+        break
+    else:
+        oldval = d
+if oldval != newval and oldval != neutval and newval != neutval:
+    prog = curval[0] / (newval[0] - oldval[0])
+    for i in range(1, 4):
+        shouldval[i] = lerp(oldval[i], newval[i], prog)
+    for i in range(4, 7):
+        shouldval[i] = lerp_angle(oldval[i], newval[i], prog)
+    _, v_x, v_y, v_z, vr_x, vr_y, vr_z = (s - c for s, c in zip(shouldval, curval))
+
 move.dLoc = [v_x, v_y, v_z]
+move.dRot = [vr_x, vr_y, vr_z]
 controller.activate(move)
+
+if space.positive:
+    print(" ".join(map(str, curval)))
--- running.py
+++ running.py
@@ -4,6 +4,7 @@ import math
 c = bge.logic.getCurrentController()
 ob = c.owner
 scene = bge.logic.getCurrentScene()
+cam = scene.objects['Camera']
 
 
 turn_phase_length = 4
@@ -18,24 +19,24 @@ total_phase_length = turn_phase_length + move_phase_length
 def set_object_rotation(rOb, degs, axis):
     xyz = rOb.localOrientation.to_euler()
     xyz[axis] = math.radians(degs)
-    rOb.localOrientation = xyz.to_matrix() 
+    rOb.localOrientation = xyz.to_matrix()
 
 def set_object_z_rotation(rOb, degs):
     """ rotation 0 means pointing in positive y direction
     rotation is counter-clockwise.
     """
-    set_object_rotation(rOb, degs, 2) 
-    
+    set_object_rotation(rOb, degs, 2)
+
 def set_object_y_rotation(rOb, degs):
     """ rotation 0 is upright
     """
-    set_object_rotation(rOb, degs, 1) 
+    set_object_rotation(rOb, degs, 1)
 
 
 def find_index(vessel_array, needle):
     """ Find the index of the vessel by the (x,y) value.
-    
-    Structure of `vessel_array` is assumed to be (game_object, 
+
+    Structure of `vessel_array` is assumed to be (game_object,
     (x,y), last_rotation).
     """
     for i, (v, pos, r) in enumerate(vessel_array):
@@ -46,7 +47,7 @@ def find_index(vessel_array, needle):
 
 def get_relevant_lines():
     """ find the lines from the log that belong to the current turn
-    
+
     sets the property ob["relevant_lines"] to the sub array of
     ob["log_lines"], and sets the property ob["end_pointer"], which is
     the index of the first line belonging to the next turn.
@@ -58,19 +59,19 @@ def get_relevant_lines():
     while curr_line != target_line and curr_line != "GAMEEND":
         ob["end_pointer"] += 1
         curr_line = ob["log_lines"][ob["end_pointer"]].split()[0]
-    
+
     # extract lines
     ob["relevant_lines"] = ob["log_lines"][ob["pointer"]:ob["end_pointer"]]
-    
+
 
 def extract_actions():
     """ gets actions from ob["relevant_lines"]
-    
+
     ob["movements"]
     ob["shots"]
     ob["deaths"]
     """
-    
+
     movements = []
     shots = []
     deaths = []
@@ -82,7 +83,7 @@ def extract_actions():
             end   = (int(fields[3]), int(fields[4]))
             mv = (start, end)
             movements.append(mv)
-            
+
         elif fields[0] == "HIT" or fields[0] == "MISS":
             # set ob["shots"] to tuples from shot start pos to end pos.
             start = (int(fields[1]), int(fields[2]))
@@ -92,7 +93,7 @@ def extract_actions():
             if fields[0] == "HIT":
                 # set ob["deaths"] to ship pos tuple
                 deaths.append(end)
-    
+
     ob["movements"] = movements
     ob["shots"] = shots
     ob["deaths"] = deaths
@@ -113,7 +114,7 @@ def compute_angles(startend):
     dy = ey-sy
     tmp = math.degrees(math.atan2(dy, dx))
     deg = wrap(0,360, - tmp - 90)
-    
+
     return deg
 
 
@@ -139,10 +140,10 @@ def lerp_pos(p1, p2, frac):
     """ like lerp but for (x,y) tuples """
     x1,y1 = p1
     x2,y2 = p2
-    
+
     xc = lerp(x1, x2, frac)
     yc = lerp(y1, y2, frac)
-    
+
     return (xc, yc)
 
 def move_object_to(obj, x, y):
@@ -153,7 +154,7 @@ def spawn_bullet(pos, rot):
     """ returns the bullet object once it's been added to the scene
     """
     blt = scene.addObject("bullet", "gameLogic")
-    blt.worldPosition = (*pos, 0.0)
+    blt.worldPosition = (pos[0], pos[1], 0.0)
     set_object_z_rotation(blt, rot)
     return blt
 
@@ -162,7 +163,7 @@ def setup():
     # these functions set global variables instead
     # of returning the results because we need to
     # keep the information accross multiple calls of
-    # the scripts which makes this a bit more 
+    # the scripts which makes this a bit more
     # difficult.
     get_relevant_lines()
     extract_actions()
@@ -174,48 +175,48 @@ def setup():
 
 def tick():
     time_in_phase = ob["frame_nr"] % total_phase_length
-    
+
     if time_in_phase == 0:
         # first frame in the new player's turn
         setup()
-        
-    
+        cam["frame_nr"] += 1
+
     vessels = ob["ships"] if ob["ship_turn"] else ob["submarines"]
-    
+
     # turn all the things!!
     if ob["turning_phase"]:
         fraction_in_turn_phase = time_in_phase / (turn_phase_length - 1)
         # need the -1 because we want to go from 0 all the way to 1, not just 0.9...
-        
+
         for pos, dir in ob["target_directions"]:
             turn_obj_index = find_index(vessels, pos)
             turn_obj, _, last_dir = vessels[turn_obj_index]
-            
+
             current_dir = lerp_angle(last_dir, dir, fraction_in_turn_phase)
-            
+
             set_object_z_rotation(turn_obj, current_dir)
-            
+
             if time_in_phase == turn_phase_length -1:
                 # update last_dir in the vessels array
                 vessels[turn_obj_index] = (turn_obj, pos, dir)
-    
-    
+
+
     # move all the things!!
     if not ob["turning_phase"]:
         fraction_in_move_phase = (time_in_phase - turn_phase_length) / (move_phase_length - 1)
-        
+
         for pos, dest in ob["movements"]:
             move_obj_index = find_index(vessels, pos)
             move_obj, start, last_dir = vessels[move_obj_index]
-            
+
             current_pos = lerp_pos(start, dest, fraction_in_move_phase)
             move_object_to(move_obj, *current_pos)
-            
+
             if time_in_phase == total_phase_length -1:
                 # update last pos in vessels array
                 vessels[move_obj_index] = (move_obj, dest, last_dir)
-        
-    
+
+
     # shoot some things!!
     # turning to align for the shots will take exactly the same time as
     # turning to move, shooting will be twice as fast as moving. I know, this
@@ -235,63 +236,64 @@ def tick():
                 blt = spawn_bullet(start, dir)
                 bullets.append((blt, (start,end)))
             ob["bullets"] = bullets
-        
+
         fraction_in_shooting_phase = time_in_shooting_phase  / (shoot_phase_length - 1)
-        
+
         for blt, path in ob["bullets"]:
-            current_pos = lerp_pos(*path, fraction_in_shooting_phase)
+            current_pos = lerp_pos(path[0], path[1], fraction_in_shooting_phase)
             move_object_to(blt, *current_pos)
-        
+
         if time_in_shooting_phase == shoot_phase_length - 1:
             # despawn bullets
             for blt, _ in ob["bullets"]:
                 blt.endObject()
-    
-    
+
+
     # kill some things!!
     # dying ships turn around their local y axis
     dying_phase_length = move_phase_length - shoot_phase_length
     time_in_dying_phase = time_in_phase - turn_phase_length - shoot_phase_length
     is_dying_phase = time_in_dying_phase >= 0
     if is_dying_phase:
-        
+
         fraction_in_dying_phase = time_in_dying_phase / (dying_phase_length - 1)
-        
+
         for pos in ob["deaths"]:
             dying_ship_index = find_index(ob["ships"], pos)
             dying_ship, _, _ = ob["ships"][dying_ship_index]
-            
+
             current_y_rotation = lerp(0, 180, fraction_in_dying_phase)
-            
+
             set_object_y_rotation(dying_ship, current_y_rotation)
-    
-    
+
+
     if time_in_phase == turn_phase_length-1:
         # reached end of turn phase, switch to move phase
         ob["turning_phase"] = False
-        
+
     if time_in_phase == total_phase_length-1:
         # reached end of player's turn, prepare for next turn
         ob["ship_turn"] = not ob["ship_turn"]
         ob["turning_phase"] = True
         ob["pointer"] = ob["end_pointer"]
 
-            
-    
+
+
 
 if "game_running" in ob and ob["game_running"]:
     # we don't need no .. optimization... dum dudum dum dum ..
     tick()
     ob["frame_nr"]+=1
-    
-    if ob["log_lines"][ob["pointer"]].split()[0] == "GAMEEND": 
+    cam["frame_nr_exact"] = ob["frame_nr"]
+
+    if ob["log_lines"][ob["pointer"]].split()[0] == "GAMEEND":
         ob["game_running"] = False
-    
-    
-    
-    
-    
+
+
+
+
+
 # example code for setting z rotation of an object
 # xyz = gameobject.localOrientation.to_euler()
 # xyz[2] = math.radians(45)
-# own.localOrientation = xyz.to_matrix() 
+# own.localOrientation = xyz.to_matrix()
